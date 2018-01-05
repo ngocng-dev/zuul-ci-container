1825ecdc0504d7f19b89e3ae65cca60fd010dfbd
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2018-01-05 22:05:10 +0000

    Add nss_wrapper to zuul executor
    In order to do ssh things, we need to use nss_wrapper as OpenShift
    sets an arbitrary UID, which does not have an entry on /etc/passwd.

---
+++ roles/ansible-role-zuul-executor-container/files/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+envsubst < /etc/passwd.template > /etc/passwd_zuul
+export LD_PRELOAD=libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/etc/passwd_zuul
+export NSS_WRAPPER_GROUP=/etc/group
+
+exec $@
